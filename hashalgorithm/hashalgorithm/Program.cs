769d80feca0124acab6db06fdd174fe5985c279d
Baris Tikir <brstkr@icloud.com>
2020-09-18 23:39:44 +0200

    added insertionsort

---
+++ hashalgorithm/hashalgorithm/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace hashalgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates and initializes the one-dimensional target Array.
+            var myTargetArray = new String[8];
+            myTargetArray[0] = "The";
+            myTargetArray[1] = "quick";
+            myTargetArray[2] = "brown";
+            myTargetArray[3] = "fox";
+            myTargetArray[4] = "jumps";
+            myTargetArray[5] = "fox";
+            myTargetArray[6] = "the";
+            myTargetArray[7] = "lazy";
+
+            var table = new Hashtable();
+
+
+            foreach (var item in myTargetArray)
+            {
+                if (!table.ContainsValue(item))
+                {
+                    table.Add(item.GetHashCode(), item);
+                }
+                else
+                {
+                    table.Add(item.GetHashCode() + 1, item);
+                }
+            }
+
+            SearchTableEntry("lazy", table);
+            DeleteTableEntry("the", table);
+            PrintHashtableValues(table);
+
+        }
+
+        public static bool SearchTableEntry(String value, Hashtable table)
+        {
+ 
+            foreach (DictionaryEntry entry in table)
+            {
+                if (entry.Value == value)
+                {
+                    Console.WriteLine("Found Entry! The Entry Key:{1} & Entry Value:{0}", entry.Value, entry.Key);
+                    return true;
+                }
+            }
+
+            Console.WriteLine("Entry Not Found!");
+            return false;
+        }
+
+        public static void DeleteTableEntry(String value, Hashtable table)
+        {
+            if (table.ContainsValue(value))
+            {
+                table.Remove(value.GetHashCode());
+                Console.WriteLine("Deleted Entry with Value: {0}", value);
+            }
+        }
+
+        public static void PrintHashtableValues(Hashtable table)
+        {
+            foreach (DictionaryEntry entry in table)
+            {
+                Console.WriteLine(entry.Key + ":" + entry.Value);
+            }
+        }
+
+        public static void PrintValues(String[] myArr, char mySeparator)
+        {
+            for (int i = 0; i < myArr.Length; i++)
+                Console.Write($"{mySeparator}{myArr[i]}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ sortalgorithm-insertion/sortalgorithm-insertion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sortalgorithm_insertion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 3, 2, 4, 7};
+            int links = 0;
+
+            if (links < array.Length)
+            {
+                for (int i = links; i < array.Length -1 ; i++)
+                {
+                    for (int k = i + 1; k > 0; k--)
+                    {
+                        if (array[k - 1] > array[k])
+                        {
+                            int j = array[k - 1];
+                            array[k - 1] = array[k];
+                            array[k] = j;
+                        }
+                    }
+
+                }
+
+                foreach (var item in array)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
