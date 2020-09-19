1cb01de15f6260dbaaefa86b9421b0cd47bec692
Baris Tikir <brstkr@icloud.com>
2020-09-19 23:32:19 +0200

    added binary tree algorithm (add+output tree elements) & added linear array searchalgorithm

---
+++ binary-tree/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace binary_tree
+{
+    public class Knoten
+    {
+        public Knoten links { get; set; }
+        public Knoten rechts { get; set; }
+        public int data { get; set; }
+    }
+
+    public class BinaryTree
+    {
+        private Knoten _root { get; set; }
+
+        public BinaryTree()
+        {
+            _root = null;
+        }
+
+        public void Ausgabe(Knoten root)
+        {
+            Console.WriteLine("Wurzel Wert:{0}", root.data);
+            Console.WriteLine("Linke Knoten Wert:{0}", root.links.data);
+            Console.WriteLine("Rechter Knoten Wert:{0}", root.rechts.data);
+        }
+
+        public Knoten Einfügen(Knoten root, string value)
+        {
+            if (root  == null)
+            {
+                root = new Knoten();
+                root.data = Convert.ToInt32(value);
+            }
+            else if (Convert.ToInt32(value) < root.data)
+            {
+                root.links = Einfügen(root.links, value);
+            }
+            else
+            {
+                root.rechts = Einfügen(root.rechts, value);
+            }
+
+            return root;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ searchalgorithm-arraycomplexsearch/searchalgorithm-arraycomplexsearch/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+// Lineare Suche im Array
+namespace searchalgorithm_arraycomplexsearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Res = new int[] { 9, 4, 2, 78, 2, 2, 1 };
+            Console.WriteLine("Res = " + linearFoo(Res,2 ,0));
+        }
+
+
+        /// <summary>
+        /// Doppelte Lineare Suche im Array nach gesuchter Zahl.
+        /// </summary>
+        /// <param name="Bar">Array mit Elementen</param>
+        /// <param name="Star">Gesuchte Zahl</param>
+        /// <param name="Megastar">Array Element Inkrement</param>
+        /// <returns>Anzahl der Gesuchten Zahl im Array</returns>
+        public static int Foo(int[] Bar, int Star, int Megastar)
+        {
+            if (Megastar > Bar.Length -1)
+            {
+                return 0;
+            }
+            if (Star == Bar[Megastar])
+            {
+                return (Foo(Bar, Star, Megastar + 1) + 1);
+            }
+            else
+            {
+                return Foo(Bar, Star, Megastar + 1);
+            }
+        }
+
+        /// <summary>
+        /// Zur Optimierung der Zeitkomplexität von dem Suchalgorithmus.
+        /// Lineare Suche im Array nach der gesuchten Zahl.
+        /// </summary>
+        /// <param name="Bar">Array mit Elementen</param>
+        /// <param name="Star">Gesuchte Zahl</param>
+        /// <param name="Megastar">Array Element Inkrement</param>
+        /// <returns>Anzahl der gesuchten Zahl im Array</returns>
+        public static int linearFoo(int[] Bar, int Star, int Megastar)
+        {
+            int count = 0;
+            for (Megastar = 0; Megastar < Bar.Length -1; Megastar++)
+            {
+                if (Star == Bar[Megastar])
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
