d1ea19d4cb81641a362748629e70a512fdfcee29
Baris Tikir <brstkr@icloud.com>
2020-09-22 00:31:00 +0200

    Added Bubblesort

--- sortalgorithm-bubblesort/Program.cs
+++ sortalgorithm-bubblesort/Program.cs
@@ -6,7 +6,31 @@ namespace sortalgorithm_bubblesort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 78, 55, 45, 98, 13 };
+            int[] sortedArray = bubbleSort(arr);
+
+            Console.WriteLine("Sorted:");
+            foreach (int p in sortedArray)
+                Console.Write(p + " ");
+            Console.Read();
+        }
+
+        static int[] bubbleSort (int[] array)
+        {
+            int temp;
+            for (int j = 0; j <= array.Length - 2; j++)
+            {
+                for (int i = 0; i <= array.Length - 2; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        temp = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+            return array;
         }
     }
 }
