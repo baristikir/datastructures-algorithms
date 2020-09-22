88b838006bdc1a2365f03af929275057915d6e58
Baris Tikir <brstkr@icloud.com>
2020-09-22 22:50:32 +0200

    Optimized Algorithm -> O(n)

--- unknownalgorithm-2/Program.cs
+++ unknownalgorithm-2/Program.cs
@@ -11,40 +11,60 @@ namespace unknownalgorithm_2
 
         static int functiond()
         {
-            int[] a = new int[] { 6, 5, 1, 8, 1000};
+            int[] a = new int[] { 6, 5, 1, 8, 1000 , 4, 25};
             int p = -1;
             int q = p;
             int r = q;
 
-            while (r < a.Length - 1)
+            //******************************
+            //*** NEW MODIFIED CODE O(n) ***
+            //******************************
+            for (int i = 0; i < a.Length; i++)
             {
-                r += 1;
-                if (a[r] >= p)
-                    p = a[r];
-            }
-
-            while (r > 0)
-            {
-                r -= 1;
-                if (a[r] > q)
+                int tmp = 0;
+                if (a[i] >= p)
                 {
-                    if (p > a[r])
-                    {
-                        q = a[r];
-                    }
+                    tmp = p;
+                    p = a[i];
+                }
+                else if (a[i] < p && a[i] > q)
+                {
+                    q = a[i];
+                }
+                if (tmp < p && q < tmp)
+                {
+                    q = tmp;
                 }
             }
 
-            if (p > q)
-            {
-                return p - q;
-            }
+            //**************************************
+            //*** OLD UNMODIFIED ALGORITHM O(n^2)***
+            //**************************************
+            //while (r < a.Length - 1)
+            //{
+            //    r += 1;
+            //    if (a[r] >= p)
+            //        p = a[r];
+            //}
+
+            //while (r > 0)
+            //{
+            //    r -= 1;
+            //    if (a[r] > q)
+            //    {
+            //        if (p > a[r])
+            //        {
+            //            q = a[r];
+            //        }
+            //    }
+            //}
+
+
             if (q > p)
             {
                 return q - p;
             }
-
-            return 0;
+            return p - q;
         }
     }
 }
